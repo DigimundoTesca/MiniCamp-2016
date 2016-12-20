133de6f2496bcc6319080808ae6a63c623a4721b
Ramses Martinez <ramses.mtz96@gmail.com>
2016-12-20 15:52:02 -0600

    Agregado los archivos basicos para el mini camp para aprender python3 PUM!

---
+++ Python/basics/basicos_python.py
@@ -0,0 +1,33 @@
+'''
+Operaciones sencillas
+'''
+# este es un comentario de una sola linea
+
+var1 = 98
+var2 = 5
+
+# Suma
+result = var1 + var2
+print('Suma:', result)
+
+# Resta
+result = var1 - var2
+mensaje = 'La resta es: {}'.format(result)
+print(mensaje)
+
+# Multiplicación
+result = var1 * var2
+print('Multiplicación: {}'.format(result))
+
+# división (Tradicional)
+result = var1 / var2
+mi_mensaje = 'La division es: %s' % (result)
+print(mi_mensaje)
+
+# división (Enteros)
+result = var1 // var2
+mensaje = 'La division en enteros es: %s' % (result)
+print(mensaje)
+
+#Potenciación
+result = var1 ** var2
--- Python/basics/variables.py
+++ Python/basics/variables.py
@@ -41,8 +41,10 @@ print('Potencia', result)
 '''
 Strings
 '''
-nombre = input('Ingresa tu nombre: ')
-edad = input('Ingresa tu edad: ')
+nombre = 'El chido'
+edad = '43'
+# nombre = input('Ingresa tu nombre: ')
+# edad = input('Ingresa tu edad: ')
 mensaje_1 = 'Hola ' + nombre + ', tu edad es de ' + edad + ' años.'
 mensaje_2 = 'Hola %s, tu edad es de %s años.' % (nombre, edad)
 mensaje_3 = 'Hola {}, tu edad es de {} años.'.format(nombre, edad)
@@ -50,3 +52,34 @@ print(mensaje_1)
 print(mensaje_2)
 print(mensaje_3)
 
+# Strings como listas
+print(mensaje_1[0])
+print(mensaje_1[8])
+print(mensaje_1[0:10])
+print(mensaje_1[0:20:2])
+print(mensaje_1[-1])
+
+
+# Formato
+s1 = 'primer texto'
+s2 = 'segundo texto'
+result = '{a} {b}'.format(a=s1, b=s2)
+print(result)
+
+
+# Búsqueda
+cadena = 'Este es un texto random'
+pos = cadena.find('un')
+print(pos)
+nueva_cadena = result.replace('e', 'E')
+print(nueva_cadena)
+
+
+"""
+Listas
+"""
+mi_lista = ['hola', 23, 23.12, True, 'Otro texto', False]
+mi_lista.append(2)
+mi_lista.insert(0, 'nueva cadena')
+mi_lista.remove(22)
+print(mi_lista)
