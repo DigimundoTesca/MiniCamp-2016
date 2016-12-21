91687816d5b4a3d18b90432097559e15b8a67676
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-21 12:33:38 -0600

    Creado archivos para listas en python con repaso de cadenas

---
+++ Python/basics/listas.py
@@ -0,0 +1,19 @@
+'''
+Repaso variables sencillas
+'''
+
+cadena = input('Ingresa una mega cadena!')
+
+# Busquedas
+
+# posicion = cadena.find('mega')
+# print(posicion)
+# posicion = cadena.find(input('inserta cadena a buscar'))
+# print(posicion)
+
+print(cadena)
+
+texto = input('Ingresa lo que quieres cambiar')
+texto2 = input('Ingresa el nuevo contenido')
+
+print(cadena.replace(texto, texto2))
