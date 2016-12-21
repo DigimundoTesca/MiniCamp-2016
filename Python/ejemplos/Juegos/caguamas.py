e5201587cb6e764239df258d896eea736196226c
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-21 15:58:15 -0600

    Arreglado el archivo de la cancion y modificado el archivo de las listas

--- Python/ejemplos/Juegos/caguamas.py
+++ Python/ejemplos/Juegos/caguamas.py
@@ -3,10 +3,10 @@
 '''
 
 def sing (b, end):
-	print(b or 'No more', 'bottle' + ('s' if b-1 else ''), end)
+	print(b or 'No omre', 'bottle' + ('s' if b-1 else ''), end)
 
 for i in range(99, 0, -1):
 	sing(i, 'of beer on the wall,')
 	sing(i, 'of beer,')
-	print('Take one down and pass it aroud,')
+	print('Take one down and pass it around,')
 	sing(i-1, 'of beer on the wall.\n')
