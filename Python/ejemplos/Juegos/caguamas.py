ac3446cee7e81425f0b274ad4cb4e632f8f9a687
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-21 03:51:58 -0600

    Reaordenados los archivos

---
+++ Python/ejemplos/Juegos/caguamas.py
@@ -0,0 +1,12 @@
+'''
+99 Cervezas song
+'''
+
+def sing (b, end):
+	print(b or 'No more', 'bottle' + ('s' if b-1 else ''), end)
+
+for i in range(99, 0, -1):
+	sing(i, 'of beer on the wall,')
+	sing(i, 'of beer,')
+	print('Take one down and pass it aroud,')
+	sing(i-1, 'of beer on the wall.\n')
