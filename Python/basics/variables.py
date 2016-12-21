13d959ab20056623a6841000b6ceaf949741e204
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-21 03:08:21 -0600

    Agregadas mas instruccione de variables en python

--- Python/basics/variables.py
+++ Python/basics/variables.py
@@ -1,15 +1,8 @@
-entero = 40
-
-flotante = 4.2
-
-print( entero, flotante)
-
 '''
 Ejemplo de operaciones sencillas
 '''
 x = 19
 y = 4
-
 
 # Suma
 result = x + y
@@ -36,8 +29,6 @@ result = x ** y
 print('Potencia', result)
 
 
-
-
 '''
 Strings
 '''
@@ -59,13 +50,11 @@ print(mensaje_1[0:10])
 print(mensaje_1[0:20:2])
 print(mensaje_1[-1])
 
-
 # Formato
 s1 = 'primer texto'
 s2 = 'segundo texto'
 result = '{a} {b}'.format(a=s1, b=s2)
 print(result)
-
 
 # Búsqueda
 cadena = 'Este es un texto random'
@@ -75,11 +64,137 @@ nueva_cadena = result.replace('e', 'E')
 print(nueva_cadena)
 
 
+
 """
 Listas
 """
 mi_lista = ['hola', 23, 23.12, True, 'Otro texto', False]
-mi_lista.append(2)
-mi_lista.insert(0, 'nueva cadena')
-mi_lista.remove(22)
+
+# Modificar un elemento
+mi_lista[3] = True
 print(mi_lista)
+
+# Conocer tamaño de la lista
+print( len(mi_lista))
+
+# Añadir un elemento al final de la lista
+mi_lista.append(2)
+print(mi_lista)
+
+# Insertar un elemento en una posición de la lsita
+mi_lista.insert(0, 'nueva cadena')
+print(mi_lista)
+
+# Eliminar un elemento de nuestra lista
+mi_lista.remove(23)
+print(mi_lista)
+
+# Saber si existe un elemento en nuestra lista
+print (23 in mi_lista)
+print ('hola' in mi_lista)
+
+# Concatenacion de listas
+L1 = [1, 2, 3, 'hola', True]
+L2 = ['ey!', 3, -12.3, False]
+print(L1 + L2)
+
+# Cocatenar n veces una lista
+L3 = ['ja', 'ja', 'ja']
+print(L3 * 5)
+
+# Porcion de una secuencia (no inclusiva)
+print('\n\nMi lista:', mi_lista)
+print( mi_lista[1:2])
+
+# Porcion de una secuencia (no inclusiva) con saltos
+print(mi_lista[1:6:1])
+print(mi_lista[1:6:2])
+
+# Busqueda
+ent_list = [4, 12, 121, -4 , -90, 0]
+
+# Elemento más pequeño en la lista
+print('Mínimo: ', min(ent_list))
+
+# Elemento más grande en la lista
+print('Máximo: ', max(ent_list))
+
+
+'''
+Tuplas
+'''
+
+mi_tupla = (23, -1212, True, (1, False, 'ja'))
+print(mi_tupla)
+print(mi_tupla[2])
+
+# Las tuplas son inmutables
+# No esta perimitdo :
+# mi_tupla[1] = 'Hola'
+
+# Longitud
+len(mi_tupla)
+
+# Empaquetado y desempaquetado
+a = 12
+b = -12.2
+c = 'Texto'
+e = a, b, c
+print(e)
+
+x, y, z = e
+print(x, y, z)
+
+
+'''
+Diccionarios 
+'''
+diccionario = {'x':1, 'y':10, 'z':-10}
+print(diccionario)
+
+dic_dias = {}
+dic_dias['Lunes'] = 100
+dic_dias['Martes'] = 1212
+dic_dias['Miercoles'] = [-100]
+dic_dias['Jueves'] = ['Hola', True]
+dic_dias['Viernes'] = []
+print(dic_dias)
+
+print(dic_dias['Jueves'])
+print(dic_dias['Jueves'][0])
+
+# Controlando errores de petición de valores
+# print(dic_dias['Domingo'])
+print(dic_dias.get('Domingo'))
+
+# Recorrer un diccionario
+for dia in dic_dias:
+	print(dia, ':', dic_dias[dia])
+
+# Impresion de valores condicionados
+for dia, codigos in dic_dias.items():
+	print(dia, ':', codigos)
+
+print(dic_dias.items())
+
+d = {'x':20, 'y':5}
+if 'x' in d:
+	print(d['x'])
+
+if 'z' in d:
+	print(d['z'])
+
+if 'y' in d:
+	print(d['y'])
+
+
+'''
+Manejo de Excepciones 
+'''
+# Esto marca un error
+# print(10/0)
+
+try:
+	result = 10 / 0
+except:
+	print('No se permite la división entre 0')
