2ef1469afe05ad7a6e97482bb2afa69d14c4a51a
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-21 16:15:16 -0600

    Creada las carpetas de los ejercicios y las instrucciones de la tarea

--- Python/basics/listas.py
+++ Python/basics/listas.py
@@ -2,7 +2,7 @@
 Repaso variables sencillas
 '''
 
-cadena = input('Ingresa una mega cadena!')
+# cadena = input('Ingresa una mega cadena!')
 
 # Busquedas
 
@@ -11,9 +11,35 @@ cadena = input('Ingresa una mega cadena!')
 # posicion = cadena.find(input('inserta cadena a buscar'))
 # print(posicion)
 
-print(cadena)
+# print(cadena)
 
-texto = input('Ingresa lo que quieres cambiar')
-texto2 = input('Ingresa el nuevo contenido')
+# texto = input('Ingresa lo que quieres cambiar')
+# texto2 = input('Ingresa el nuevo contenido')
 
-print(cadena.replace(texto, texto2))
+# print(cadena.replace(texto, texto2))
+
+
+
+
+# L = [1,2,3,4,5,6]
+
+# for valor in L:
+# 	print(valor)
+
+
+
+DIAS = {
+	'lunes': 100, 
+	'martes': 200, 
+	'miercoles': 3434,
+	'jueves': 213, 
+	'viernes': 0, 
+	'sabado': 'No voa trabajar :v ... a veces', 
+}
+
+
+
+for dia in DIAS:
+	print('Dia de la semana: ', DIAS[dia])
+
+
--- Python/ejercicios-alumnos/enteros/Enteros.py
+++
@@ -1,9 +0,0 @@
-'''
-Cosas sencillas con enteros
-'''
-var1 = 4
-var2 = 2
-
-var3 = var1 - var2
-result = (var1 + var2)//var3
-print('El resultado en enteros es: {}'.format(result))
