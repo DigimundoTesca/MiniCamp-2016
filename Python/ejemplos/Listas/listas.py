c26f8826e4d4ef657b8dc2a18a11e035c11a37a8
Ramsés Martinez <ramses.mtz96@gmail.com>
2016-12-22 01:40:31 -0600

    agregago el archivo de listas

---
+++ Python/ejemplos/Listas/listas.py
@@ -0,0 +1,13 @@
+'''
+Programa que realizará la tarea del método find
+'''
+lista = ['1','2','3']
+c = input('Ingresa lo que quieres buscar\t')
+# def busqueda(lista):
+#   for valor in range(0,len(lista)):
+#       print(valor)
+tama = len(lista)
+
+for i in lista:
+    if i == c:
+        print(c)
