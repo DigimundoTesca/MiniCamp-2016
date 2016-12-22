f461b3f1f21dae37cf8051c01b64dcca17121b0f
Baruch <dolce_dmn@hotmail.com>
2016-12-22 01:25:24 -0600

    Creado el juego piedra papel tijeras lagarto spock, siguiendo el ejemplo visto en clase, además de terminar la busqueda en listas, se añadió además el intento de volver recursivo el proyecto

--- Python/ejercicios-alumnos/Busqueda/Lineal/lineal.py
+++ Python/ejercicios-alumnos/Busqueda/Lineal/lineal.py
@@ -1,13 +1,17 @@
 '''
 Programa que realizará la tarea del método find
 '''
-lista = [1,2,3]
+lista = ['1','2','3']
 c = input('Ingresa lo que quieres buscar\t')
-# def busqueda(lista):
-# 	for valor in range(0,len(lista)):
-# 		print(valor)
-tama = len(lista)
-for i in lista:
-	if lista == c:
-		print(c)
+def busqueda(lista):
+	for i in lista:
+		if i == c:
+			print('True')
+		elif i != c:
+			print('False')
+			break
 
+#Intento de hacer recursiva la busqueda
+# opc = input('¿Nueva busqueda?(y/n)')
+# while opc == 'y':
+	busqueda(lista)
