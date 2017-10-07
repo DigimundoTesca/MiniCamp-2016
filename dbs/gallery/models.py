b7dc40b49dc1d59b30c762d07012a454e3b4655d
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-10-07 14:48:05 -0500

    First commit

---
+++ dbs/gallery/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=48)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return '%s' % self.name
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=48)
+    description = models.TextField(null=True, blank=True, default="Describe la tarjeta")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Tarjeta"
+        verbose_name_plural = "Tarjetas"
+
+    def __str__(self):
+        return '%s' % self.name
